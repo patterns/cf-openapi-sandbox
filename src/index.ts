d133375e1b5447287cf84612345b07d8d58245c1
興怡 <>
2025-05-06 17:18:02 -0700

    minimal data test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+
+import { SiteList, SiteFetch, SiteCreate } from "./endpoints/sites";
+import { UserList, UserFetch, UserCreate } from "./endpoints/users";
+
+
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
@@ -14,10 +15,12 @@ const openapi = fromHono(app, {
 });
 
 // Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+openapi.get("/api/sites", SiteList);
+openapi.post("/api/sites", SiteCreate);
+openapi.get("/api/sites/:id", SiteFetch);
+openapi.get("/api/users", UserList);
+openapi.post("/api/users", UserCreate);
+openapi.get("/api/users/:id", UserFetch);
 
 // You may also register routes for non OpenAPI directly on Hono
 // app.get('/test', (c) => c.text('Hono!'))
