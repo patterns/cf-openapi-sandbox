7fe6b957960edef7145d6dece57251bf01476ea0
興怡 <>
2025-05-16 17:41:33 -0700

    can we add wildcard with origin list

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const loggingMiddleware = async (c, next) => {
     console.log(`${method} ${url} - ${status} - ${duration}ms`);
 };
 const corsOptions = {
-    origin: ['http://localhost', 'http://localhost:3000', 'http://localhost:3002'],
+    origin: ['http://localhost', 'http://localhost:3000', 'http://localhost:3002', '*'],
     allowHeaders: ['Content-Type'],
     maxAge: 86400,
     credentials: true,
